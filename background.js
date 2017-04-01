b8f7043cbf41d9191fcd5f6b1cb8e9d338ad899e
Cosmin Serdean <cosmoserdean@gmail.com>
2017-04-01 13:24:12 +0300

    fasd

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 //The code that is fired upon page load
 //to check your plugin js is working uncomment the next line.
 //$("body").append('Test');
-$('body').css('transform', 'rotate(0)');
+var re = /.*\.42\.fr/;
+if (re.test(window.location.hostname))
+	$('body').css('transform', 'rotate(0)');
---
+++ t.js
@@ -0,0 +1,6 @@
+var hostname = 'intra.42.fr'
+var re = /.*\.42\.fr/;
+console.log('uau');
+if (re.test(hostname))
+	console.log('uu')
+//		$('body').css('transform', 'rotate(0)');
